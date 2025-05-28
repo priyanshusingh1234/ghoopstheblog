c11b9c16f0f3284cfde3bbf94a742d91b330ee03
Priyanshu Kumar <kpk22128@gmail.com>
2025-05-28 11:20:44 +0530

    new robo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,17 @@
 const siteMetadata = require("./src/utils/siteMetaData");
 
-
 module.exports = {
-    siteUrl: siteMetadata.siteUrl,
-    generateRobotsTxt: true,
-  }
+  siteUrl: siteMetadata.siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: "*", allow: "/" },
+      // You can add more policies here, for example:
+      // { userAgent: "*", disallow: "/secret" },
+    ],
+    additionalSitemaps: [
+      `${siteMetadata.siteUrl}/sitemap.xml`,
+      // add more sitemap URLs if needed
+    ],
+  },
+};
