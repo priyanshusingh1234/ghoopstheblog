2849a2df860c63152bb26d24d773f7351a26f8de
Priyanshu Kumar <kpk22128@gmail.com>
2025-06-29 17:18:33 +0530

    gfdgdfg

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,5 +1,5 @@
 const siteMetadata = {
-  title: 'Ghoops! | The Blog - A blog for everyone!',
+  title: ' Ghoops – AI, Dev & Productivity Insights Blog!',
   author: 'Priyanshu',
   headerTitle: 'GhoopstheBlog',
   description: 'A blog providing you with the best possible articles across various topics.',
