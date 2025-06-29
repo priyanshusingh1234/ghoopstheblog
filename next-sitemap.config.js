cc70c467e3401724a770ff3c5f67e9573002692f
Priyanshu Kumar <kpk22128@gmail.com>
2025-06-29 13:06:56 +0530

    ygtft

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: 'https://ghoopstheblog.vercel.app',
+  siteUrl: 'https://ghoopstheblog.me',
   generateRobotsTxt: false, // Disable auto-generation
-  sitemap: 'https://ghoopstheblog.vercel.app/sitemap.xml',
+  sitemap: 'https://ghoopstheblog.me/sitemap.xml',
 }
